d6ea21bbc48d98f281187d7aae46372fbcba3672
HyungGyu Ryoo <hgryoo@pnu.edu>
2016-01-08 23:17:23 +0900

    change original FeatureTypeRegistry to copied NewFeatureTypeRegistry

--- modules/extension/xsd/xsd-indoorgml2/src/test/java/IndoorCoreParsingTest.java
+++ modules/extension/xsd/xsd-indoorgml2/src/test/java/IndoorCoreParsingTest.java
@@ -1,21 +1,24 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Set;
 
 import javax.xml.namespace.QName;
 
+import org.geotools.data.complex.NewFeatureTypeRegistry;
 import org.geotools.data.complex.config.EmfComplexFeatureReader;
 import org.geotools.data.complex.config.FeatureTypeRegistry;
+import org.geotools.factory.CommonFactoryFinder;
+import org.geotools.factory.GeoTools;
+import org.geotools.feature.FeatureCollection;
+import org.geotools.feature.FeatureIterator;
 import org.geotools.feature.NameImpl;
 import org.geotools.feature.complex.ComplexFeatureGraphGenerator;
 import org.geotools.feature.complex.NewXmlComplexFeatureParser;
 import org.geotools.feature.type.ComplexFeatureTypeFactoryImpl;
+import org.geotools.filter.text.cql2.CQL;
 import org.geotools.gml3.complex.GmlFeatureTypeRegistryConfiguration;
 import org.geotools.graph.build.line.LineStringGraphGenerator;
 import org.geotools.graph.path.DijkstraShortestPathFinder;
@@ -27,13 +30,13 @@ import org.geotools.graph.traverse.standard.DijkstraIterator;
 import org.geotools.graph.traverse.standard.DijkstraIterator.EdgeWeighter;
 import org.geotools.xml.resolver.SchemaResolver;
 import org.junit.Test;
-import org.opengis.feature.ComplexAttribute;
 import org.opengis.feature.Feature;
 import org.opengis.feature.GeometryAttribute;
-import org.opengis.feature.Property;
 import org.opengis.feature.type.AttributeDescriptor;
 import org.opengis.feature.type.FeatureType;
-import org.opengis.feature.type.Name;
+import org.opengis.filter.Filter;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.identity.FeatureId;
 
 import com.vividsolutions.jts.geom.Geometry;
 
@@ -51,7 +54,7 @@ public class IndoorCoreParsingTest {
             SchemaResolver appSchemaResolver = new SchemaResolver();
             EmfComplexFeatureReader reader = EmfComplexFeatureReader.newInstance();
             reader.setResolver(appSchemaResolver);
-            FeatureTypeRegistry typeRegistry = new FeatureTypeRegistry(new ComplexFeatureTypeFactoryImpl(),
+            NewFeatureTypeRegistry typeRegistry = new NewFeatureTypeRegistry(new ComplexFeatureTypeFactoryImpl(),
                         new GmlFeatureTypeRegistryConfiguration(null));
             typeRegistry.addSchemas(reader.parse(schemaLocation));
             
@@ -59,7 +62,9 @@ public class IndoorCoreParsingTest {
                             .getDescriptor(new NameImpl("http://www.opengis.net/indoorgml/1.0/core",
                                             ":", "IndoorFeatures"), null);
             FeatureType featureType = (FeatureType) descriptor.getType();
-    
+            
+            typeRegistry.disposeSchemaIndexes();
+            
             // Creating Feature
             URL url = getClass().getResource("indoor.gml");
             NewXmlComplexFeatureParser featureParser = new NewXmlComplexFeatureParser(
@@ -71,16 +76,49 @@ public class IndoorCoreParsingTest {
             Feature feature = featureParser.parse();
             
             FeatureTableGenerator ftg = new FeatureTableGenerator(feature);
-            List tfs = ftg.getFeatureCollection( 
+            FeatureCollection tfs = ftg.getFeatureCollection( 
                     new NameImpl("http://www.opengis.net/indoorgml/1.0/core","Transition"));
+            
+            FeatureIterator it = tfs.features();
+            
+            FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2( GeoTools.getDefaultHints() );
+
+            /*Set<FeatureId> fids = new HashSet<FeatureId>();
+            fids.add( ff.featureId("T10") );
+            fids.add( ff.featureId("T19") );
+            Filter filter = ff.id( fids );*/
+            
+            Filter filter = CQL.toFilter("name == 002");
+            
+            Object o = filter.evaluate(tfs);
+            
+            ArrayList<Feature> features = new ArrayList<Feature>();
+            try {
+                while( it.hasNext() ){
+                    Feature f = it.next();
+                    if(filter.evaluate(f)) {
+                        features.add(f);
+                    }
+                    // process feature
+                }
+            }
+            finally {
+                it.close();
+            }
+            
+            System.out.println();
+            
+            
+           //Filter filter = CQL.toFilter("name >= 5");
+            
             
             //create a linear graph generate
             LineStringGraphGenerator lineStringGen = new LineStringGraphGenerator();
             ComplexFeatureGraphGenerator featureGen = new ComplexFeatureGraphGenerator( lineStringGen );
             
             for(int i = 0 ; i < tfs.size(); i++) {
-                Feature transition = (Feature) tfs.get(i);
-                featureGen.add(transition);
+                //Feature transition = (Feature) tfs.get(i);
+                //featureGen.add(transition);
             }
             
             Graph graph = lineStringGen.getGraph();
