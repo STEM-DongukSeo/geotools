75340fe61fc2a5390d852313a344a0c6a4d6818e
HyungGyu Ryoo <hgryoo@pnu.edu>
2016-03-31 20:53:54 +0900

    Added hashCode and toString in ISOGeometryAttributeImpl class

--- modules/library/main-new/src/main/java/org/geotools/feature/ISOGeometryAttributeImpl.java
+++ modules/library/main-new/src/main/java/org/geotools/feature/ISOGeometryAttributeImpl.java
@@ -16,12 +16,14 @@
  */
 package org.geotools.feature;
 
+import org.geotools.util.Utilities;
 import org.opengis.feature.GeometryAttribute;
 import org.opengis.feature.type.GeometryDescriptor;
 import org.opengis.feature.type.GeometryType;
 import org.opengis.filter.identity.Identifier;
 import org.opengis.geometry.BoundingBox;
 import org.opengis.geometry.Geometry;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 /**
  * The GeometryAttribute Implementation providing geometry model based on ISO19107
@@ -73,22 +75,108 @@ public class ISOGeometryAttributeImpl extends AttributeImpl implements GeometryA
         this.bounds = bbox;
     }
 
+    public boolean equals(Object o) {
+        if ( this == o ) {
+            return true;
+        }
+        
+            if (!(o instanceof ISOGeometryAttributeImpl)) {
+                    return false;
+            }
+            
+            ISOGeometryAttributeImpl att = (ISOGeometryAttributeImpl) o;
+
+            if (!Utilities.equals(descriptor, att.descriptor))
+                    return false;
+
+            if (!Utilities.equals(id, att.id))
+                    return false;
+
+            //TODO
+            if ( value != null && att.value != null ) {
+                // another lovley jts thing... comparing geometry collections that 
+                // arent multi point/line/poly throws an exception, so we need to 
+                // do that comparison
+                
+                /*
+                if ( att.value instanceof GeometryCollection && 
+                !(att.value instanceof MultiPoint) && 
+                !(att.value instanceof MultiLineString) &&
+                !(att.value instanceof MultiPolygon) ) {
+                    
+                    if ( value instanceof GeometryCollection ) {
+                        //compare the two collections 
+                        GeometryCollection c1 = (GeometryCollection) value;
+                        GeometryCollection c2 = (GeometryCollection) att.value;
+                        
+                        if ( c1.getNumGeometries() !=  c2.getNumGeometries() ) {
+                            return false;
+                        }
+                        
+                        for ( int i = 0; i < c1.getNumGeometries(); i++ ) {
+                            Geometry g1 = c1.getGeometryN(i);
+                            Geometry g2 = c2.getGeometryN(i);
+                            
+                            if ( !g1.equalsExact(g2) ) {
+                                return false;
+                            }
+                        }
+                        
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
+                }
+                */
+                if ( !((Geometry)value).equals((Geometry)att.value)) {
+                            return false;
+                    }
+            }
+            else {
+                return Utilities.deepEquals(value, this.value);    
+            }
+            
+            return true;
+    }
+
     @Override
     public int hashCode() {
-        // TODO Auto-generated method stub
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        // TODO Auto-generated method stub
-        return super.equals(obj);
-    }
-
+        int hash = descriptor.hashCode(); 
+        
+        if ( id != null ) {
+            hash += 37 * id.hashCode();
+        }
+        
+        return hash;
+}
+    
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        return super.toString();
+        StringBuffer sb = new StringBuffer(getClass().getSimpleName()).append(":");
+        sb.append(getDescriptor().getName().getLocalPart());
+        CoordinateReferenceSystem crs = getDescriptor().getType().getCoordinateReferenceSystem();
+        if(!getDescriptor().getName().getLocalPart().equals(getDescriptor().getType().getName().getLocalPart()) ||
+                id != null || crs != null){
+            sb.append("<");
+            sb.append(getDescriptor().getType().getName().getLocalPart());
+            if( id != null ){
+                sb.append( " id=");
+                sb.append( id );
+            }
+            if( crs != null ){
+                sb.append( " crs=");
+                sb.append( crs );
+            }
+        if( id != null ){
+            sb.append( " id=");
+            sb.append( id );
+        }
+        sb.append(">");
+        }
+        sb.append("=");
+        sb.append(value);
+        return sb.toString();
     }
     
 }
